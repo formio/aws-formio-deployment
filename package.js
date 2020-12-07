f90cdb1eecdb4e624209ec2daaddd65c93d68fe2
Travis Tidwell <travis@form.io>
2020-12-06 20:52:10 -0600

    Updated package

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
-const SERVER_VERSION = 'formio/formio-enterprise:7.0.0-rc.56';
-const PDF_VERSION = 'formio/pdf-server:3.0.0-rc.27';
+const SERVER_VERSION = 'formio/formio-enterprise:7.0.0-rc.59';
+const PDF_VERSION = 'formio/pdf-server:3.0.0-rc.28';
 const child_process = require("child_process");
 const fs = require('fs');
 console.log('Reading .Dockerrun.aws.json');
